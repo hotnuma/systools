ec9057643113e6215dc6552d1c58e04e2ed97020
hotnuma <hotnuma@gmail.com>
2025-03-06 02:08:41 +0100

    builddep: show error

--- install.sh
+++ install.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/bash
 
-meson setup build -Dbuildtype=plain
+basedir="$(dirname -- "$(readlink -f -- "$0";)")"
+opt_clean=0
+buildtype="plain"
+
+while (($#)); do
+    case "$1" in
+        clean)
+        opt_clean=1
+        ;;
+        -type)
+        buildtype="debug"
+        ;;
+        *)
+        ;;
+    esac
+    shift
+done
+
+dest=$basedir/build
+if [[ $opt_clean == 1 && -d $dest ]]; then
+    rm -rf $dest
+fi
+
+meson setup build -Dbuildtype=${buildtype}
 sudo meson install -C build
 
 
