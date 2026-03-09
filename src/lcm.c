123628c15d258786a83c348aa47e9dbd3bbe8cf1
hotnuma <hotnuma@gmail.com>
2026-03-09 11:09:54 +0100

    lcm: add lcm struct

--- src/lcm.c
+++ src/lcm.c
@@ -3,7 +3,14 @@
 #include <libapp.h>
 #include <math.h>
 
-#define APPNAME "lcr"
+#define APPNAME "lcm"
+
+typedef struct _Lcm
+{
+    double C1;
+    double L1;
+
+} Lcm;
 
 static void usage_exit()
 {
@@ -16,16 +23,16 @@ static void usage_exit()
     exit(EXIT_FAILURE);
 }
 
-static void _config_get_path(CString *result)
+static void _lcm_get_path(CString *result)
 {
     get_homedir(result);
     cstr_append(result, "/.config/lcm.conf");
 }
 
-static bool config_read(double *C1, double *L1)
+static bool lcm_read(Lcm *lcm)
 {
     CStringAuto *inipath = cstr_new_size(64);
-    _config_get_path(inipath);
+    _lcm_get_path(inipath);
 
     CIniFileAuto *inifile = cinifile_new();
 
@@ -44,22 +51,22 @@ static bool config_read(double *C1, double *L1)
     value = strtod(c_str(temp), NULL);
     if (value == 0)
         return false;
-    *C1 = value;
+    lcm->C1 = value;
 
     if (!cinisection_key_value(section, temp, "L1", ""))
         return false;
     value = strtod(c_str(temp), NULL);
     if (value == 0)
         return false;
-    *L1 = value;
+    lcm->L1 = value;
 
     return true;
 }
 
-static bool config_save(double C1, double L1)
+static bool lcm_save(Lcm *lcm)
 {
     CStringAuto *inipath = cstr_new_size(64);
-    _config_get_path(inipath);
+    _lcm_get_path(inipath);
     
     CFileAuto *file = cfile_new();
 
@@ -68,9 +75,9 @@ static bool config_save(double C1, double L1)
 
     CStringAuto *temp = cstr_new_size(32);
     cfile_write(file, "[LCM]\n");
-    cstr_fmt(temp, "C1=%ld\n", C1);
+    cstr_fmt(temp, "C1=%ld\n", lcm->C1);
     cfile_write(file, c_str(temp));
-    cstr_fmt(temp, "L1=%ld\n", L1);
+    cstr_fmt(temp, "L1=%ld\n", lcm->L1);
     cfile_write(file, c_str(temp));
     cfile_write(file, "\n");
 
@@ -85,7 +92,7 @@ static void print_result(char *fname, double result)
         printf("%s: %lf\n", fname, result);
 }
 
-int func_cal(int argc, char **argv)
+int lcm_cal(Lcm *lcm, int argc, char **argv)
 {
     if (argc != 4)
         usage_exit();
@@ -97,35 +104,36 @@ int func_cal(int argc, char **argv)
     double f2 = strtod(argv[3], NULL);
     double f1_2 = pow(f1, 2);
     double f2_2 = pow(f2, 2);
-    double C1 = Cref*f2_2/(f1_2-f2_2);
-    double L1 = 1/(4*pow(M_PI, 2)*f1_2*C1);
-
-    print_result("C1", C1);
-    print_result("L1", L1);
     
-    config_save(C1, L1);
+    lcm->C1 = Cref*f2_2/(f1_2-f2_2);
+    lcm->L1 = 1/(4*pow(M_PI, 2)*f1_2*lcm->C1);
+
+    print_result("C1", lcm->C1);
+    print_result("L1", lcm->L1);
+    
+    lcm_save(lcm);
     
     return EXIT_SUCCESS;
 }
 
-int func_c(int argc, char **argv, double C1)
+int lcm_c(Lcm *lcm, int argc, char **argv)
 {
     if (argc != 4)
         usage_exit();
     
     double f1 = strtod(argv[2], NULL);
     double f3 = strtod(argv[3], NULL);
-    double result = C1*((pow(f1, 2)/pow(f3, 2)) - 1);
+    double result = lcm->C1*((pow(f1, 2)/pow(f3, 2)) - 1);
     
     print_result(argv[1], result);
 
     return EXIT_SUCCESS;
 }
 
-int func_l(int argc, char **argv, double C1)
+int lcm_l(Lcm *lcm, int argc, char **argv)
 {
+    (void) lcm;
     (void) argv;
-    (void) C1;
     
     if (argc != 4)
         usage_exit();
@@ -135,22 +143,23 @@ int func_l(int argc, char **argv, double C1)
 
 int main(int argc, char **argv)
 {
-    double C1 = 1.076879e-09;
-    double L1 = 100e-6;
+    Lcm lcm;
+    lcm.C1 = 1.076879e-09;
+    lcm.L1 = 100e-6;
     
-    config_read(&C1, &L1);
+    lcm_read(&lcm);
 
     if (argc < 2)
         usage_exit();
 
     if (strcmp(argv[1], "cal") == 0)
-        return func_cal(argc, argv);
+        return lcm_cal(&lcm, argc, argv);
     
     if (strcmp(argv[1], "c") == 0)
-        return func_c(argc, argv, C1);
+        return lcm_c(&lcm, argc, argv);
     
     if (strcmp(argv[1], "l") == 0)
-        return func_l(argc, argv, C1);
+        return lcm_l(&lcm, argc, argv);
     
     return EXIT_FAILURE;
 }
