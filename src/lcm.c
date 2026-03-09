3ebaded3e639ee49067aa6f1eca1949e2d9d5ac5
hotnuma <hotnuma@gmail.com>
2026-03-09 05:12:43 +0100

    lcm: read write config file

--- src/lcm.c
+++ src/lcm.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cinifile.h>
+#include <cfile.h>
+#include <libapp.h>
 #include <math.h>
 
 #define APPNAME "lcr"
@@ -16,7 +16,59 @@ static void usage_exit()
     exit(EXIT_FAILURE);
 }
 
-void print_result(char *fname, double result)
+static void _config_get_path(CString *result)
+{
+    get_homedir(result);
+    cstr_append(result, "/.config/lcm.conf");
+}
+
+static bool config_read(double *result)
+{
+    CStringAuto *inipath = cstr_new_size(64);
+    _config_get_path(inipath);
+
+    CIniFileAuto *inifile = cinifile_new();
+
+    if (!cinifile_read(inifile, c_str(inipath)))
+        return false;
+
+    CIniSection *section = cinifile_section(inifile, "LCM");
+    if (!section)
+        return false;
+
+    CStringAuto *temp = cstr_new_size(12);
+    if (!cinisection_key_value(section, temp, "cal", ""))
+        return false;
+
+    double val = strtod(c_str(temp), NULL);
+    if (val == 0)
+        return false;
+    
+    *result = val;
+
+    return true;
+}
+
+static bool config_save(double value)
+{
+    CStringAuto *inipath = cstr_new_size(64);
+    _config_get_path(inipath);
+    
+    CFileAuto *file = cfile_new();
+
+    if (!cfile_open(file, c_str(inipath), "wb"))
+        return false;
+
+    CStringAuto *temp = cstr_new_size(32);
+    cfile_write(file, "[LCM]\n");
+    cstr_fmt(temp, "cal=%ld\n", value);
+    cfile_write(file, c_str(temp));
+    cfile_write(file, "\n");
+
+    return true;
+}
+
+static void print_result(char *fname, double result)
 {
     if (result > -1 && result < 1)
         printf("%s: %.6e\n", fname, result);
@@ -37,12 +89,12 @@ int func_cal(int argc, char **argv)
 
     print_result(argv[1], result);
     
+    config_save(result);
+    
     return EXIT_SUCCESS;
 }
 
-double C1 = 1.076879e-09;
-
-int func_c(int argc, char **argv)
+int func_c(int argc, char **argv, double C1)
 {
     if (argc != 4)
         usage_exit();
@@ -56,8 +108,11 @@ int func_c(int argc, char **argv)
     return EXIT_SUCCESS;
 }
 
-int func_l(int argc, char **argv)
+int func_l(int argc, char **argv, double C1)
 {
+    (void) argv;
+    (void) C1;
+    
     if (argc != 4)
         usage_exit();
     
@@ -66,6 +121,10 @@ int func_l(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
+    double C1 = 1.076879e-09;
+    
+    config_read(&C1);
+
     if (argc < 2)
         usage_exit();
 
@@ -73,10 +132,10 @@ int main(int argc, char **argv)
         return func_cal(argc, argv);
     
     if (strcmp(argv[1], "c") == 0)
-        return func_c(argc, argv);
+        return func_c(argc, argv, C1);
     
     if (strcmp(argv[1], "l") == 0)
-        return func_l(argc, argv);
+        return func_l(argc, argv, C1);
     
     return EXIT_FAILURE;
 }
